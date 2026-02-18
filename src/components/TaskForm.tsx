e04e777e94eff7726ab6d8c38b13eb6de3bdaeee
anle10 <hongan101003@gmail.com>
2026-02-18 23:21:21 +0200

    Save data into Tasks

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,20 +1,62 @@
 import React from "react";
 import { useTasks } from "../context/TaskContext";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type { Task } from "../type";
 
 type Props = {};
+
+const schema = z.object({
+  title: z.string().min(2, "Title too short"),
+  assignedTo: z.number(),
+});
+type FormData = z.infer<typeof schema>;
 
 const TaskForm = (props: Props) => {
   const {
     state: { users },
+    dispatch,
   } = useTasks();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = (data: FormData) => {
+    const newTask: Task = {
+      id: Date.now(),
+      title: data.title,
+      done: false,
+      assignedTo: data.assignedTo,
+    };
+
+    dispatch({
+      type: "add",
+      payload: newTask,
+    });
+  };
   return (
-    <form className="flex gap-2 bg-gray-700 p-4 rounded mb-4">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex gap-2 bg-gray-700 p-4 rounded mb-4"
+    >
       <input
+        {...register("title")}
         type="text"
         placeholder="Task title"
         className="flex-1 p-2 rounded border bg-gray-800 border-gray-700"
       ></input>
-      <select className="p-2 rounded border-gray-700 bg-gray-800">
+      <select
+        {...register("assignedTo", {
+          valueAsNumber: true,
+        })}
+        className="p-2 rounded border-gray-700 bg-gray-800"
+      >
         {users.map((u) => (
           <option key={u.id} value={u.id}>
             {u.name}
@@ -27,6 +69,10 @@ const TaskForm = (props: Props) => {
       >
         ADD
       </button>
+
+      {errors.title && (
+        <span className="text-red-500 text-sm">{errors.title.message}</span>
+      )}
     </form>
   );
 };
